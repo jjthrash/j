c95e4e53c73b549db37aa7e0b25ab7540c6cbfae
Justin Dubs <jtdubs@dubs-w500.dubs.homeunix.org>
2009-02-19 10:02:30 -0500

    change j to be a shell replacement

--- j.sh
+++ j.sh
@@ -1,72 +1,55 @@
-# maintains a jump-list of directories you actually use
-# old directories eventually fall off the list
-# inspired by http://wiki.github.com/joelthelion/autojump
-# and something similar i had - but i could never get the dir list right.
-#
-# INSTALL:
-#   source into .bashrc under your '[-z "$PS1" ] || return' line
-#   cd around for a while
-#
-# USE:
-#   j [--l] [regex1 ... regexn]
-#     regex1 ... regexn jump to the most used directory matching all masks
-#     --l               show the list instead of jumping
-#                       with no args, returns full list
-j() {
- # change jfile if you already have a .j file for something else
- jfile=$HOME/.j
- if [ "$1" = "--add" ]; then
-  shift
-  # we're in $HOME all the time, let something else get all the good letters
-  [ "$*" = "$HOME" ] && return
-  awk -v q="$*" -v mx=1000 -F"|" '
-   $2 >= 1 { 
-    if( $1 == q ) { l[$1] = $2 + 1; x = 1 } else l[$1] = $2
-    y += $2
-   }
-   END {
-    x || l[q] = 1
-    if( y > mx ) {
-     for( i in l ) print i "|" l[i]*(0.9*mx/y) # aging
-    } else for( i in l ) print i "|" l[i]
-   }
-  ' $jfile 2>/dev/null > $jfile.tmp
-  mv $jfile.tmp $jfile
- elif [ "$1" = "" ];then
-  j ~
- elif [ "$1" = "--l" ];then
-  shift
-  awk -v q="$*" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) print $2 "\t" $1 }
-  ' $jfile 2>/dev/null | sort -n
- # for completion
- elif [ "$1" = "--complete" ];then
-  awk -v q="$3" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) print $1 }
-  ' $jfile 2>/dev/null
- # if we hit enter on a completion just go there
- elif [ "${1:0:1}" = "/" -a -d "$*" ]; then
-  cd "$*" 
- else
-  # prefer case sensitive
-  cd=$(awk -v q="$*" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) { print $2 "\t" $1; x = 1 } }
-   END {
-    if( x ) exit
-    close(FILENAME)
-    while( getline < FILENAME ) {
-     for( o in a ) tolower($1) !~ tolower(a[o]) && $1 = ""
-     if( $1 ) print $2 "\t" $1
-    }
-   }
-  ' $jfile 2>/dev/null | sort -nr | head -n 1 | cut -f 2)
-  [ "$cd" ] && cd "$cd"
- fi
+#!/bin/bash
+
+inc() {
+  perl -pi -e '
+    ($n, $d) = split /\s/;
+    $_ = ($n+1) . " $d\n" if $d eq "'`pwd`'";
+  ' ~/.j
 }
-# prepend to PROMPT_COMMAND
-PROMPT_COMMAND='j --add "$(pwd -P)";'"$PROMPT_COMMAND"
-# bash completions for j
-complete -o dirnames -o filenames -C "j --complete" j
+
+add_or_inc() {
+  if grep -w `pwd` ~/.j
+  then
+    inc
+  else
+    echo 1 `pwd` >> ~/.j
+  fi
+}
+
+grep_all() {
+  if [ "$*" == "" ]
+  then
+    cat -
+  else
+    pattern=$1
+    shift
+    egrep "$pattern" - | grep_all ${*}
+  fi
+}
+
+j() {
+  touch ~/.j
+
+  if [ "$*" = "" ]
+  then
+    builtin cd
+  elif [ -d $1 -o "$1" = "-" ]
+  then
+    builtin cd $1
+    add_or_inc
+  elif [ "$1" = "-l" ]
+  then
+    sort -nr ~/.j
+  else
+    for dir in `cat ~/.j | grep_all $* | sort -nr | uniq | cut -d" " -f2`
+    do
+      if [ -d $dir ]
+      then
+        builtin cd $dir
+        inc
+        return
+      fi
+    done
+    echo no directory found matching: $*
+  fi
+}
